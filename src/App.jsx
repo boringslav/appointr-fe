a8c7aed88a7779449ce5ee0045e1af5dec8d2553
Borislav Stoyanov <bostoy@protonmail.com>
2022-04-25 23:05:54 +0200

    Create mui theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
 import Booking from "./pages/Booking/Booking";
 import Bookings from "./pages/Bookings/Bookings";
 import Home from "./pages/Home/Home";
@@ -10,23 +12,28 @@ import SignIn from "./pages/SignIn/SignIn";
 import SignUp from "./pages/SignUp/SignUp";
 
 function App() {
+
+  const theme = createTheme();
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path="bookings">
-          <Route index element={<Bookings />} />
-          <Route path=":bookingId" element={<Booking />} />
-          <Route path="new" element={<CreateBooking />} />
-        </Route>
-        <Route path="users">
-          <Route index element={<Users />} />
-          <Route path=":userId" element={<User />} />
-        </Route>
-        <Route path="sign-in" element={<SignIn />} />
-        <Route path="sign-up" element={<SignUp />} />
-      </Routes>
-    </BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path="bookings">
+              <Route index element={<Bookings />} />
+              <Route path=":bookingId" element={<Booking />} />
+              <Route path="new" element={<CreateBooking />} />
+            </Route>
+            <Route path="users">
+              <Route index element={<Users />} />
+              <Route path=":userId" element={<User />} />
+            </Route>
+            <Route path="sign-in" element={<SignIn />} />
+            <Route path="sign-up" element={<SignUp />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
   );
 }
 
