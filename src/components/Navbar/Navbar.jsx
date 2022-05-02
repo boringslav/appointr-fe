e8dc08b66d27990a86ceecf77143f6a16dc56624
Borislav Stoyanov <bostoy@protonmail.com>
2022-05-02 16:38:42 +0200

    Split NotLoggedInNavbar into a separate file

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,37 +1,15 @@
 import {useContext} from 'react';
-import {NavLink} from "react-router-dom";
-import {AppBar, Button, Toolbar, Typography} from "@mui/material";
-import Box from "@mui/material/Box";
-import {styled} from "@mui/system";
+import UserContext from "../../context/UserContext";
+import NavbarNotLoggedIn from "./NavbarNotLoggedIn/NavbarNotLoggedIn";
 
-const StyledLink = styled(NavLink)`
-  text-decoration: none;
-  color: white;
 
-  &:focus, &:hover, &:visited, &:link, &:active {
-    text-decoration: none;
-  }
-`;
 
 const Navbar = () => {
+    const user = useContext(UserContext);
+
 
 return (
-    <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-            <Toolbar>
-                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                    Appointr
-                </Typography>
-                <StyledLink to='/sign-in'>
-                    <Button color="inherit">Sign In</Button>
-                </StyledLink>
-                <StyledLink to='/sign-up'>
-                    <Button href="/sign-up" color="inherit">Sign Up</Button>
-                </StyledLink>
-            </Toolbar>
-        </AppBar>
-    </Box>
+    <NavbarNotLoggedIn/>
 )
-
 }
 export default Navbar;
