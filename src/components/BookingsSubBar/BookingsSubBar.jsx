01813b18ab68490eb6b0873d4e966f9ad0ba827f
Borislav Stoyanov <bostoy@protonmail.com>
2022-05-03 17:01:15 +0200

    Create Bookings page subBar

---
+++ src/components/BookingsSubBar/BookingsSubBar.jsx
@@ -0,0 +1,85 @@
+import {alpha, styled} from "@mui/system";
+import {AppBar, Button, InputBase, Toolbar} from "@mui/material";
+import Box from "@mui/material/Box";
+import SearchIcon from '@mui/icons-material/Search';
+import StyledLink from "../Navbar/StyledLink/StyledLink";
+
+const Search = styled('div')(({ theme }) => ({
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: "rgba(236, 236, 236 ,1)",
+    '&:hover': {
+        backgroundColor: "rgba(236, 236, 236 ,1)",
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing(1),
+        width: 'auto',
+    },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+    '& .MuiInputBase-input': {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            width: '12ch',
+            '&:focus': {
+                width: '20ch',
+            },
+        },
+    },
+}));
+
+
+const BookingsSubBar = () => {
+
+
+    const handleKeyPress = (e) => {
+        if(e.key === "Enter") {
+            console.log("Enter Pressed");
+        }
+    }
+    return (
+        <>
+            <Box sx={{ flexGrow: 1}}>
+                <AppBar color="inherit" position="static">
+                    <Toolbar sx={{justifyContent: "space-between"}}>
+                        <Search onKeyDown={handleKeyPress}>
+                            <SearchIconWrapper>
+                                <SearchIcon />
+                            </SearchIconWrapper>
+                            <StyledInputBase
+                                placeholder="Search…"
+                                inputProps={{ 'aria-label': 'search' }}
+                            />
+                        </Search>
+                        <div>
+                            <StyledLink to="/bookings/new">
+                                <Button color="primary">
+                                    Create a Booking
+                                </Button>
+                            </StyledLink>
+                        </div>
+                    </Toolbar>
+                </AppBar>
+            </Box>
+        </>
+    )
+}
+export default BookingsSubBar;
--- src/pages/Bookings/Bookings.jsx
+++ src/pages/Bookings/Bookings.jsx
@@ -3,6 +3,7 @@ import styled from "@emotion/styled";
 import {getAllReq} from "../../services/api";
 import {Button, Card, CardActions, CardContent, Typography} from "@mui/material";
 import UserContext from "../../context/UserContext";
+import BookingsSubBar from "../../components/BookingsSubBar/BookingsSubBar";
 
 const BookingCardsContainer = styled.div`
   display: flex;
@@ -13,62 +14,62 @@ const BookingCardsContainer = styled.div`
 
 
 function Bookings() {
-  const [bookings, setBookings] = useState();
-  const [error, setError] = useState();
-  const {user} = useContext(UserContext);
+    const [bookings, setBookings] = useState();
+    const [error, setError] = useState();
+    const {user} = useContext(UserContext);
 
-  useEffect(() => {
+    useEffect(() => {
 
-    (async () => {
-      try {
-        console.log("Token: ", user.access_token);
-        const response = await getAllReq("/bookings", user.access_token);
+        (async () => {
+            try {
+                const response = await getAllReq("/bookings", user.access_token);
 
-        if(response.data) {
-          setBookings(response.data.bookings);
-          console.log(bookings);
-        }
-        if(response.error) {
-          throw new Error("Ooops something went wrong!");
-        }
+                if (response.data) {
+                    setBookings(response.data.bookings);
+                }
+                if (response.error) {
+                    throw new Error("Ooops something went wrong!");
+                }
 
-      }catch (e) {
-          setError(e.message);
-      }
-    })()
+            } catch (e) {
+                setError(e.message);
+            }
+        })()
 
 
-  }, [])
+    }, [])
 
-  return <BookingCardsContainer>
+    return (
+        <>
+            <BookingsSubBar/>
+            <BookingCardsContainer>
+                {bookings && bookings.map((booking) => {
+                        return <Card key={booking.id} sx={{
+                            maxWidth: 275,
+                            margin: "2rem"
+                        }}>
+                            <CardContent>
+                                <Typography sx={{fontSize: 14}} color="text.secondary" gutterBottom>
+                                    {booking.creator.name}
+                                </Typography>
+                                <Typography variant="h5" component="div">
+                                    {booking.title}
+                                </Typography>
 
-    {bookings && bookings.map((booking) => {
-        return  <Card key={booking.id} sx={{
-          maxWidth: 275,
-          margin: "2rem"
-        }}>
-          <CardContent>
-            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-              {booking.creator.name}
-            </Typography>
-            <Typography variant="h5" component="div">
-              {booking.title}
-            </Typography>
-
-            <Typography variant="body2">
-              {booking.description}
-              <br />
-              {'"a benevolent smile"'}
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <Button size="small">Book</Button>
-          </CardActions>
-        </Card>
-        }
-      )}
-
-  </BookingCardsContainer>;
+                                <Typography variant="body2">
+                                    {booking.description}
+                                    <br/>
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Button size="small">Book</Button>
+                            </CardActions>
+                        </Card>
+                    }
+                )}
+            </BookingCardsContainer>;
+        </>
+    )
 }
 
 export default Bookings;
